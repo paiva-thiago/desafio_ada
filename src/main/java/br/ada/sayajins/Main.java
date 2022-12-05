fefe7bfb657b6939aeb17a26f90ee1dab9c9c13a
Isaac Gomes Medeiros <visac123@gmail.com>
2022-12-05 16:04:18 +0000

    CSV mapeado para uma lista de pagamentos

--- src/main/java/br/ada/sayajins/Main.java
+++ src/main/java/br/ada/sayajins/Main.java
@@ -1,8 +1,47 @@
 package br.ada.sayajins;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ada.sayajins.model.Pagamentos;
+import br.ada.sayajins.model.TipoPagamentoEnum;
+
 public class Main {
     public static void main(String[] args) {
+        String file = "src/main/resources/pagamentos.csv";
+        List<String> content = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while ((br.readLine()) != null) {
+                content.add(br.readLine());                
+            }
+        }
+        catch(IOException e) {
+          e.printStackTrace();
+        }    
 
+        List<Pagamentos> listaPagamentos = new ArrayList<>();
+
+        for(int i = 0; i< content.size(); i++){
+
+            String[] arrOfStr = content.get(i).split(";");
+
+            DateTimeFormatter JEFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            LocalDate data = LocalDate.parse(arrOfStr[2], JEFormatter);
+            TipoPagamentoEnum tipo = TipoPagamentoEnum.valueOf(arrOfStr[1]);
+            double valor = Double.parseDouble(arrOfStr[3]);
+            String nome = arrOfStr[0];
+            listaPagamentos.add(new Pagamentos(nome, data, valor, tipo));
+
+        }
+
+        for(Pagamentos a : listaPagamentos){
+            System.out.println(a);
+        }
     }
 
 }
